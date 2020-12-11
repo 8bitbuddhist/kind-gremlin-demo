fa0b55a11f240b0dcd6c75ac0a08c92dd8e07153
Andre Newman <10094408+8bitbuddhist@users.noreply.github.com>
2020-12-11 17:41:59 -0500

    Added options to not install Gremlin or app in run.sh

--- run.sh
+++ run.sh
@@ -1,35 +1,79 @@
 #!/bin/bash
+NO_GREMLIN=0
+NO_APP=0
+
+function usage() {
+	echo "Usage: run.sh [ --no-gremlin ] [--no-app] cluster-name"
+	echo "Options:"
+	echo "	-h | --help	Show this help screen."
+	echo "	--no-app 	Don't deploy the Online Boutique demo application."
+	echo "	--no-gremlin	Don't deploy Gremlin."
+	exit 2
+}
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+		--no-gremlin)
+			NO_GREMLIN=1
+			shift
+			;;
+		--no-app)
+			NO_APP=1
+			shift
+			;;
+		--help|-h)
+			usage
+			shift
+			;;
+		*)
+			break
+			;;
+	esac
+done
+
+if [ -z "$1" ]; then
+	echo "Cluster name required."
+	usage
+	exit
+else
+	KIND_CLUSTER_NAME=$1
+fi
+
 source .env
 
 # Create the kind cluster
 kind delete cluster --name ${KIND_CLUSTER_NAME}
 kind create cluster --name ${KIND_CLUSTER_NAME} --config config.yaml
 
-# Detect Gremlin certificate files
-GREMLIN_CERT_FILE=$(ls -1 ${GREMLIN_CERT_PATH}/*cert.pem | head -n 1)
-GREMLIN_KEY_FILE=$(ls -1 ${GREMLIN_CERT_PATH}/*key.pem | head -n 1)
+if [ $NO_GREMLIN -eq 0 ]; then
+	# Detect Gremlin certificate files
+	GREMLIN_CERT_FILE=$(ls -1 ${GREMLIN_CERT_PATH}/*cert.pem | head -n 1)
+	GREMLIN_KEY_FILE=$(ls -1 ${GREMLIN_CERT_PATH}/*key.pem | head -n 1)
 
-# Create a Gremlin namespace and secret
-kubectl create ns gremlin
-kubectl -n gremlin create secret generic gremlin-team-cert \
-	--from-file=gremlin.cert=${GREMLIN_CERT_FILE} \
-	--from-file=gremlin.key=${GREMLIN_KEY_FILE} \
-	--from-literal=GREMLIN_TEAM_ID=${GREMLIN_TEAM_ID} \
-	--from-literal=GREMLIN_CLUSTER_ID=${GREMLIN_CLUSTER_ID}
+	# Create a Gremlin namespace and secret
+	kubectl create ns gremlin
+	kubectl -n gremlin create secret generic gremlin-team-cert \
+		--from-file=gremlin.cert=${GREMLIN_CERT_FILE} \
+		--from-file=gremlin.key=${GREMLIN_KEY_FILE} \
+		--from-literal=GREMLIN_TEAM_ID=${GREMLIN_TEAM_ID} \
+		--from-literal=GREMLIN_CLUSTER_ID=${GREMLIN_CLUSTER_ID}
 
-# Deploy Gremlin
-helm repo add gremlin https://helm.gremlin.com
-helm install gremlin gremlin/gremlin \
-	--namespace gremlin \
-	--set gremlin.secret.name=gremlin-team-cert \
-	--set gremlin.hostPID=true \
-	--set gremlin.container.driver=containerd-runc
+	# Deploy Gremlin
+	helm repo add gremlin https://helm.gremlin.com
+	helm install gremlin gremlin/gremlin \
+		--namespace gremlin \
+		--set gremlin.secret.name=gremlin-team-cert \
+		--set gremlin.hostPID=true \
+		--set gremlin.container.driver=containerd-runc
+fi
 
 # Deploy an Nginx Ingress controller and wait for it to rollout
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
 kubectl rollout status deployment/ingress-nginx-controller -n ingress-nginx
 
 # Deploy the demo application
-kubectl create ns microservices-demo
-kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/master/release/kubernetes-manifests.yaml -n microservices-demo
-kubectl apply -f microservices-demo-ingress.yaml -n microservices-demo
+if [ $NO_APP -eq 0 ]; then
+	kubectl create ns microservices-demo
+	kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/master/release/kubernetes-manifests.yaml -n microservices-demo
+	kubectl apply -f microservices-demo-ingress.yaml -n microservices-demo
+fi
